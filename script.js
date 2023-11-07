78089f9695a0fd83684d0511f89c445051219201
“Pedro <pedrodomicio@gmail.com>
2023-11-07 20:33:06 -0300

    Teste

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const audioElements = [
-    document.getElementById('audio1'),
-    document.getElementById('audio2'),
-    document.getElementById('audio3'),
-    document.getElementById('audio4'),
-    document.getElementById('audio5')
-];
+function setVolume(audioId, volume) {
+    const audio = document.getElementById(audioId);
+    audio.volume = volume;
+}
+
+const audioElements = document.querySelectorAll('audio');
+
+let isPlaying = false; // Add a variable to track if audio is playing
+
 let soloed = [];
 
 function toggleSolo(audioId) {
@@ -61,8 +63,8 @@ function toggleMute(audioId) {
     audio.muted = !audio.muted;
     const muteButton = document.getElementById('muteButton' + audioId.charAt(audioId.length - 1));
     muteButton.classList.toggle('active', audio.muted);
+    console.log(`Toggled mute for audio ${audioId}. Muted: ${audio.muted}`);
 }
-
 
 function playAllAudio() {
     const playAllButton = document.getElementById('playAllButton');
@@ -72,23 +74,63 @@ function playAllAudio() {
     audioElements.forEach(function (audio) {
         audio.play();
     });
+    isPlaying = true;
+    console.log('All audio played.');
 }
 
+// Add a function to stop all audio
 function stopAllAudio() {
     const playAllButton = document.getElementById('playAllButton');
     playAllButton.classList.remove('active');
+    isPlaying = false; // Set isPlaying to false
     
-    var audioElements = document.querySelectorAll('audio');
     audioElements.forEach(function (audio) {
         audio.pause();
         audio.currentTime = 0;
     });
+    console.log('All audio stopped.');
 }
 
-function setVolume(audioId, volume) {
-    var audio = document.getElementById(audioId);
+// Add an event listener to the stop button
+const stopAllButton = document.getElementById('stopAllButton');
+stopAllButton.addEventListener('click', function () {
+    if (isPlaying) { // Check if audio is playing
+        stopAllAudio();
+    }
+    console.log('Stop button clicked.');
+});
+
+// Add an event listener to handle audio play
+audioElements.forEach((audio) => {
+    audio.addEventListener('canplay', () => {
+        if (isPlaying) { // Check if audio is supposed to be playing
+            audio.play();
+        }
+    });
+    audio.load(); // Start loading the audio
+    console.log(`Audio element ${audio.id} loaded.`);
+});
+
+// Function to set the custom volume
+function setCustomVolume(audioId, volume) {
+    const audio = document.getElementById(audioId);
     audio.volume = volume;
 }
+
+// Attach event listeners for both mouse and touch events to use the custom volume control
+audioElements.forEach((audio) => {
+    const audioId = audio.id;
+    const volumeInput = document.getElementById('volume' + audioId.charAt(audioId.length - 1));
+
+    volumeInput.addEventListener('input', () => {
+        setCustomVolume(audioId, volumeInput.value);
+    });
+
+    volumeInput.addEventListener('touchstart', () => {
+        setCustomVolume(audioId, volumeInput.value);
+    });
+    console.log(`Volume control attached for audio ${audioId}`);
+});
 
 function updateLabels() {
     var audioElements = document.querySelectorAll('audio');
@@ -99,6 +141,7 @@ function updateLabels() {
         var label = labels[i];
         label.textContent = decodeURIComponent(getFileName(audio.src));
     }
+    console.log('Updated volume labels.');
 }
 
 function getFileName(src) {
