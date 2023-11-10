87cf56754e6609ee41f15c73909157638e3779b2
“Pedro <pedrodomicio@gmail.com>
2023-11-09 22:31:05 -0300

    Implementando howler.js

--- script.js
+++ script.js
@@ -1,146 +1,149 @@
-const audioElements = document.querySelectorAll('audio');
+const audioTracks = [
+    {
+        src: 'http://localhost:8080/Aviao%20Arpeggio%201.m4a',
+        volume: 1.0,
+        loop: true,
+    },
+    {
+        src: 'http://localhost:8080/Aviao%20Arpeggio%202.m4a',
+        volume: 1.0,
+        loop: true,
+    },
+    {
+        src: 'http://localhost:8080/Aviao%20Bass.m4a',
+        volume: 1.0,
+        loop: true,
+    },
+    {
+        src: 'http://localhost:8080/Aviao%20Drums.m4a',
+        volume: 1.0,
+        loop: true,
+    },
+    {
+        src: 'http://localhost:8080/Aviao%20Organ.m4a',
+        volume: 1.0,
+        loop: true,
+    },
+];
 
-let isPlaying = false; // Add a variable to track if audio is playing
+const soundObjects = [];
 
-let soloed = [];
-
-function toggleSolo(audioId) {
-    const audio = document.getElementById(audioId);
-    const soloButton = document.getElementById('soloButton' + audioId.charAt(audioId.length - 1));
-
-    if (soloed.includes(audioId)) {
-        // Toggle off solo for the current track
-        soloed = soloed.filter((id) => id !== audioId);
-        audio.muted = false;
-        soloButton.classList.remove('active');
-        
-        if (soloed.length === 0) {
-            // If there are no other soloed tracks, unmute all tracks
-            audioElements.forEach((element) => {
-                element.muted = false;
-                const muteButton = document.getElementById('muteButton' + element.id.charAt(element.id.length - 1));
-                muteButton.classList.remove('active');
-            });
-        } else {
-            // Mute only non-soloed tracks
-            audioElements.forEach((element) => {
-                const muteButton = document.getElementById('muteButton' + element.id.charAt(element.id.length - 1));
-                if (!soloed.includes(element.id)) {
-                    element.muted = true;
-                    muteButton.classList.add('active');
-                } else {
-                    element.muted = false;
-                    muteButton.classList.remove('active');
-                }
-            });
-        }
-    } else {
-        // Toggle on solo for the current track
-        soloed.push(audioId);
-        soloButton.classList.add('active');
-
-        // Mute only non-soloed tracks
-        audioElements.forEach((element) => {
-            const muteButton = document.getElementById('muteButton' + element.id.charAt(element.id.length - 1));
-            if (!soloed.includes(element.id)) {
-                element.muted = true;
-                muteButton.classList.add('active');
-            } else {
-                element.muted = false;
-                muteButton.classList.remove('active');
-            }
+function initializeAudioTracks(audioContext) {
+    audioTracks.forEach((track, index) => {
+        soundObjects[index] = new Howl({
+            src: [track.src],
+            volume: track.volume,
+            loop: track.loop,
+            preload: true,
+            ctx: audioContext, // Pass the audio context
         });
-    }
+    });
 }
 
-function toggleMute(audioId) {
-    const audio = document.getElementById(audioId);
-    audio.muted = !audio.muted;
-    const muteButton = document.getElementById('muteButton' + audioId.charAt(audioId.length - 1));
-    muteButton.classList.toggle('active', audio.muted);
-    console.log(`Toggled mute for audio ${audioId}. Muted: ${audio.muted}`);
+document.getElementById('startAudioButton').addEventListener('click', () => {
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    initializeAudioTracks(audioContext);
+});
+
+// Function to update the volume for a specific track
+function setCustomVolume(trackIndex, volume) {
+    if (soundObjects[trackIndex]) {
+        soundObjects[trackIndex].volume(volume);
+    }
 }
 
 function playAllAudio() {
-    const playAllButton = document.getElementById('playAllButton');
-    playAllButton.classList.add('active');
-
-    var audioElements = document.querySelectorAll('audio');
-    audioElements.forEach(function (audio) {
-        audio.play();
-    });
+    soundObjects.forEach((sound) => sound.play());
     isPlaying = true;
-    console.log('All audio played.');
 }
 
-// Add a function to stop all audio
 function stopAllAudio() {
-    const playAllButton = document.getElementById('playAllButton');
-    playAllButton.classList.remove('active');
-    isPlaying = false; // Set isPlaying to false
-    
-    audioElements.forEach(function (audio) {
-        audio.pause();
-        audio.currentTime = 0;
-    });
-    console.log('All audio stopped.');
+    soundObjects.forEach((sound) => sound.stop());
+    isPlaying = false;
 }
 
-// Add an event listener to handle audio play
-audioElements.forEach((audio) => {
-    audio.addEventListener('canplay', () => {
-        if (isPlaying) { // Check if audio is supposed to be playing
-            audio.play();
+function toggleMute(trackIndex, button) {
+    if (soundObjects[trackIndex]) {
+        if (soundObjects[trackIndex]._muted) {
+            // Unmute the track
+            soundObjects[trackIndex].volume(audioTracks[trackIndex].volume);
+            button.innerHTML = '<i class="fas fa-volume-up"></i>';
+        } else {
+            // Mute the track
+            soundObjects[trackIndex].volume(0);
+            button.innerHTML = '<i class="fas fa-volume-mute"></i>'; // Change the button icon
+        }
+        soundObjects[trackIndex]._muted = !soundObjects[trackIndex]._muted;
+    }
+}
+
+// Create an array to keep track of soloed tracks
+const soloedTracks = [];
+
+// Function to toggle solo for a track
+function toggleSolo(trackIndex, button) {
+    const isSoloed = soloedTracks.includes(trackIndex);
+  
+    // If the track is already soloed, unsolo it
+    if (isSoloed) {
+        const index = soloedTracks.indexOf(trackIndex);
+        if (index !== -1) {
+            soloedTracks.splice(index, 1);
+        }
+    } else {
+        // If the track is not soloed, solo it and add to the soloedTracks array
+        soloedTracks.push(trackIndex);
+    }
+  
+    // Update the volume for all tracks based on the solo state
+    soundObjects.forEach((sound, index) => {
+        if (soloedTracks.length === 0) {
+            sound.volume(audioTracks[index].volume);
+        } else {
+            sound.volume(soloedTracks.includes(index) ? audioTracks[index].volume : 0);
         }
     });
-    audio.load(); // Start loading the audio
-    console.log(`Audio element ${audio.id} loaded.`);
-});
 
-// Function to set the custom volume
-function setCustomVolume(audioId, volume) {
-    const audio = document.getElementById(audioId);
-    audio.volume = volume;
+    // Toggle the button appearance
+    if (isSoloed) {
+        button.innerHTML = '<i class="fas fa-microphone"></i>'; // Change the button icon when unsoloed
+    } else {
+        button.innerHTML = '<i class="fas fa-microphone-slash"></i>'; // Change the button icon when soloed
+    }
 }
 
-// Attach event listeners for both mouse and touch events to use the custom volume control
-audioElements.forEach((audio) => {
-    const audioId = audio.id;
-    const volumeInput = document.getElementById('volume' + audioId.charAt(audioId.length - 1));
-
-    volumeInput.addEventListener('input', () => {
-        setCustomVolume(audioId, volumeInput.value);
+document.addEventListener('DOMContentLoaded', () => {
+    const startAudioButton = document.getElementById('startAudioButton');
+    startAudioButton.addEventListener('click', () => {
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        initializeAudioTracks(audioContext);
     });
 
-    volumeInput.addEventListener('touchstart', () => {
-        setCustomVolume(audioId, volumeInput.value);
+    // Add your HTML and control elements here
+
+    const playAllButton = document.getElementById('playAllButton');
+    playAllButton.addEventListener('click', playAllAudio);
+
+    const stopAllButton = document.getElementById('stopAllButton');
+    stopAllButton.addEventListener('click', stopAllAudio);
+
+    // Initialize volume labels
+    updateLabels();
+
+    // Event listener to start playing audio when ready
+    soundObjects.forEach((sound, index) => {
+        sound.on('load', () => {
+            if (isPlaying) {
+                sound.play();
+            }
+        });
     });
-    console.log(`Volume control attached for audio ${audioId}`);
 });
 
 function updateLabels() {
-    console.log('Updating labels...'); // Add this line for debugging
-    var audioElements = document.querySelectorAll('audio');
-    var labels = document.querySelectorAll('.volume-label');
-    
-    for (var i = 0; i < audioElements.length; i++) {
-        var audio = audioElements[i];
-        var label = labels[i];
-        label.textContent = decodeURIComponent(getFileName(audio.src));
-    }
-    console.log('Updated volume labels.');
-}
+    const labels = document.querySelectorAll('.volume-label');
 
-function getFileName(src) {
-    var index = src.lastIndexOf('/');
-    if (index !== -1) {
-        src = src.substring(index + 1);
-    }
-    index = src.lastIndexOf('.');
-    if (index !== -1) {
-        src = src.substring(0, index);
-    }
-    return src;
+    labels.forEach((label, index) => {
+        label.textContent = audioTracks[index].src;
+    });
 }
-
-updateLabels();
